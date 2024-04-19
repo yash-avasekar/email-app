843e5e4d9e2ea46ddc591dcac663c47c9c0077e7
yash-avasekar <stevefrayne3@gmail.com>
2024-04-19 17:39:05 +0530

    create app password in gmail two-set-verification

--- API_Email/API_Email/settings.py
+++ API_Email/API_Email/settings.py
@@ -131,8 +131,9 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "yash.avasekar.1999@gmail.com"
+EMAIL_HOST_USER = "exmaple@gmail.com"
 EMAIL_HOST_PASSWORD = "ivqp xgfr wpcj gcbh"
 
+# create app password in gmail by enabling two step verification
 # app password
 # ivqp xgfr wpcj gcbh
